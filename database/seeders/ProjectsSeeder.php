3c2a1d1f259451ca7f8146a37fc42c7935667a8c
mikzanco <mikyzancolo97@gmail.com>
2023-01-18 17:30:46 +0100

    [create] seeder and create projects folder

--- database/seeders/ProjectsSeeder.php
+++ database/seeders/ProjectsSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Projects;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,17 @@ class ProjectsSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        for($i = 0; $i < 100; $i++){
+            $new_project = new Projects();
+            $new_project->name = $faker->sentence();
+            $new_project->slug = Projects::generateSlug($new_project->name);
+            $new_project->client_name = $faker->sentence();
+            $new_project->summary = $faker->paragraph(5);
+            $new_project->cover_image = 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg';
+            $new_project->save();
 
+        }
     }
 }
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,6 +6,6 @@
 
 @section('content')
 <div class="container">
-    <h1>Elenco dei progetti</h1>
+    <h1>Questa è la dashboard di {{ Auth::user()->name }}</h1>
 </div>
 @endsection
