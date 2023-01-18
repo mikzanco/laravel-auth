91b486a45a62a5c36e2a96036742473d6777382d
mikzanco <mikyzancolo97@gmail.com>
2023-01-18 17:48:32 +0100

    create aside part, alcuni stili e pulito l'header

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,5 @@
+<nav>
+    <ul class="p-0 mt-3">
+        <li><a class="mz-nav-link" href="#"><i class="fa-solid fa-chart-line me-1"></i> Dashboard</a></li>
+    </ul>
+</nav>
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -33,12 +33,9 @@
                 @endif
                 @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
-                    </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('admin.dashboard') }}">Admin</a>
+
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
