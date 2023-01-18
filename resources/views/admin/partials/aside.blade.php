66b57a1c3a8bd6e21c835d908c57d977c78240ad
mikzanco <mikyzancolo97@gmail.com>
2023-01-18 17:55:14 +0100

    fix header container-fluid, add a in aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,5 +1,6 @@
 <nav>
     <ul class="p-0 mt-3">
         <li><a class="mz-nav-link" href="#"><i class="fa-solid fa-chart-line me-1"></i> Dashboard</a></li>
+        <li><a class="mz-nav-link" href="#"><li><i class="fa-solid fa-diagram-project"></i></i> Project</a></li>
     </ul>
 </nav>
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
+    <div class="container-fluid">
         <a class="navbar-brand d-flex align-items-center" href="{{ route('home') }}">
             <div class="logo_laravel">
                 PORTFOLIO ZANCO
